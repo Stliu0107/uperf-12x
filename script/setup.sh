134d44e6051baf15a97c8dc65f4a1cd0eef6d8c2
lzx010725 <61444320+Stliu0107@users.noreply.github.com>
2026-04-19 14:32:00 +0800

    chore: update installer version to 2026.04.19.14

--- script/setup.sh
+++ script/setup.sh
@@ -2,23 +2,38 @@
 #
 # Copyright (C) 2021-2026 12X
 #
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
 BASEDIR="$(dirname $(readlink -f "$0"))"
 . $BASEDIR/pathinfo.sh
 . $BASEDIR/libsysinfo.sh
 
+# $1:error_message
 abort() {
     echo "$1"
     echo "! Uperf installation failed."
     exit 1
 }
 
+# $1:file_node $2:owner $3:group $4:permission $5:secontext
 set_perm() {
     chown $2:$3 $1
     chmod $4 $1
     chcon $5 $1
 }
 
+# $1:directory $2:owner $3:group $4:dir_permission $5:file_permission $6:secontext
 set_perm_recursive() {
     find $1 -type d 2>/dev/null | while read dir; do
         set_perm $dir $2 $3 $4 $6
@@ -33,6 +48,8 @@ install_uperf() {
     echo "- ro.board.platform=$(getprop ro.board.platform)"
     echo "- ro.product.board=$(getprop ro.product.board)"
 
+    # 12X build is dedicated for 8EG5 (SM8850/Kaanapali). Use fixed profile
+    # to avoid install-time detection failures across OEM property variants.
     local cfgname="sdm8g5"
     [ -f "$MODULE_PATH/config/$cfgname.json" ] || abort "! Missing $cfgname.json in module package."
 
@@ -59,7 +76,7 @@ install_uperf() {
 echo ""
 echo "* Uperf-12X"
 echo "* Author: 12X"
-echo "* Version: 2026.04.18.21"
+echo "* Version: 2026.04.19.14"
 echo ""
 
 echo "- Installing uperf"
